9d83597e006e615429c933f3944ab3b038c85f9c
Aaron Becker <aaron_becker@external.mckinsey.com>
2024-02-09 12:16:38 -0500

    add some rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,34 @@ module.exports = {
   env: {
     node: true,
   },
+  rules: {
+    "padding-line-between-statements": [
+      "error",
+      {
+        blankLine: "always",
+        prev: [
+          "multiline-block-like",
+          "multiline-expression",
+          "multiline-const",
+        ],
+        next: "*",
+      },
+    ],
+    "sort-imports": [
+      "error",
+      {
+        ignoreDeclarationSort: true,
+      },
+    ],
+    eqeqeq: "error",
+    "object-shorthand": "error",
+    "no-unneeded-ternary": ["error", { defaultAssignment: false }],
+    "no-implicit-coercion": "error",
+    "no-debugger": "error",
+    "prefer-template": "error",
+    "no-shadow": "warn",
+    "no-void": ["error", { allowAsStatement: true }],
+  },
   overrides: [
     {
       files: ["tests/**/*.js"],
