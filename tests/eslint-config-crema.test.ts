5297b90c9a42c0ceccaa5e98545d754bb34912be
Aaron Becker <aaron_becker@external.mckinsey.com>
2024-06-14 11:56:55 -0400

    update test

--- tests/eslint-config-crema.test.ts
+++ tests/eslint-config-crema.test.ts
@@ -2,20 +2,20 @@ import { ESLint } from "eslint"
 
 import { describe, it, expect } from "vitest"
 
-const plugin = new ESLint({
-    overrideConfigFile: "../crema-lint/lib/recommended.js",
-    overrideConfig: {
-        settings: {
-            react: {
-                //https://github.com/DRD4-7R/eslint-config-7r-building/issues/1#issuecomment-473031376
-                //without this there is a stderr cluttering the test logs
-                version: "999.999.999",
+describe("eslint-config-crema recommended config", () => {
+    const plugin = new ESLint({
+        overrideConfigFile: "./lib/recommended.js",
+        overrideConfig: {
+            settings: {
+                react: {
+                    //https://github.com/DRD4-7R/eslint-config-7r-building/issues/1#issuecomment-473031376
+                    //without this there is a stderr cluttering the test logs
+                    version: "999.999.999",
+                },
             },
         },
-    },
-})
+    })
 
-describe("eslint-config-crema", () => {
     it("should prefer object shorthand", async () => {
         const code = `
             const test = 1;
